393d65558aaa81566ada5fa94f1be36f94c84cb0
pankajproject99 <pankajproject99@gmail.com>
2022-12-27 20:06:45 +0530

    Added new url with JSON response

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+db_courses = [
+    {'name': "python", 'course_id': "0", 'Description': "Python Programming", 'price': "1.0"},
+    {'name': "java", 'course_id': "1", 'Description': "Java Programming", 'price': "2.0"},
+    {'name': "C", 'course_id': "2", 'Description': "C Programming", 'price': "3.0"},
+    {'name': ".Net", 'course_id': "3", 'Description': ".Net Programming", 'price': "4.0"},
+]
+
 
 @app.route('/')
 def index():
     return "Welcome To The Course API"
+
+
+@app.route("/courses", methods=['GET'])
+def get_courses():
+    return jsonify({'Courses': db_courses})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
