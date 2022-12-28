8950c4d07731d6ce112c41f95013b54094d5f5d6
pankajproject99 <pankajproject99@gmail.com>
2022-12-28 16:34:08 +0530

    Django return object via serializable

--- mydjango/api/views.py
+++ mydjango/api/views.py
@@ -1,7 +1,12 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from base.models import Item
+from .serializers import ItemSerializer
 
 @api_view(['GET'])
 def getData(request):
-    person = {'name': 'Pankaj', 'age': 28}
-    return Response(person)
+    # person = {'name': 'Pankaj', 'age': 28}
+    # return Response(person)
+    items = Item.objects.all()
+    serializer = ItemSerializer(items, many=True)
+    return Response(serializer.data)
